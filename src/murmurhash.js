c529b6d3c5902a88969726f88a60d231fcaf5c29
Brendan Ashworth <brendan.ashworth@me.com>
2015-04-14 18:11:15 -0700

    fix passing falsy values as seed
    This commit provides a fix for providing a falsy value as a seed to the
    hash function. Previously it would then default to 1, but now it uses
    the correct seed.
    
    An example of this would be using zero.

--- src/murmurhash.js
+++ src/murmurhash.js
@@ -3,8 +3,11 @@ var binding = require('../build/Release/murmurhash');
 exports.hash = function(key, seed) {
     if (typeof key !== 'string')
         throw new TypeError('key argument must be a string');
+
     if (seed !== undefined && typeof seed !== 'number')
         throw new TypeError('seed argument must be a number');
+    else if (seed === undefined)
+        seed = 1;
 
-    return binding.hash(key, seed || 1);
+    return binding.hash(key, seed);
 };
--- test/main.js
+++ test/main.js
@@ -39,6 +39,8 @@ describe('node-murmurhash3', function() {
             assert.equal(murmurhash.hash('hello, world!', 283), 3198928967)
             assert.equal(murmurhash.hash('abcdefghijkl', 23), 2275871593);
             assert.equal(murmurhash.hash('brendan ashworth', 100), 2444978076);
+            assert.equal(murmurhash.hash('brendan ashworth', -1), 861264453);
+            assert.equal(murmurhash.hash('brendan ashworth', 0), 3210849834);
         });
     });
 });
